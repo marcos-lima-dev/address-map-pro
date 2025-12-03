7afdd065a132dc115a11b801d4622a109bebb4ea
Marcos de Sousa Lima <sousa.lima@gmail.com>
2025-12-03 17:45:29 -0300

    fix: force update theme provider import

--- src/components/providers/ThemeProvider.tsx
+++ src/components/providers/ThemeProvider.tsx
@@ -1,8 +1,10 @@
 "use client";
 
 import { ThemeProvider as NextThemesProvider } from "next-themes";
-import { type ThemeProviderProps } from "next-themes"; // <--- TEM QUE SER ASSIM
+import { type ThemeProviderProps } from "next-themes"; // <--- Import corrigido
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
+
+// Correção para o deploy na Vercel v1
