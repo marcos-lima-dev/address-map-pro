9ec554327cad249f553141b3c192c63533445de7
Marcos de Sousa Lima <sousa.lima@gmail.com>
2025-12-03 17:25:35 -0300

    fix: remove dist types import from theme provider

--- src/components/providers/ThemeProvider.tsx
+++ src/components/providers/ThemeProvider.tsx
@@ -1,8 +1,7 @@
 "use client";
 
 import { ThemeProvider as NextThemesProvider } from "next-themes";
-// A correção é remover o "/dist/types" do final
-import { type ThemeProviderProps } from "next-themes";
+import { type ThemeProviderProps } from "next-themes"; // <--- TEM QUE SER ASSIM
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
